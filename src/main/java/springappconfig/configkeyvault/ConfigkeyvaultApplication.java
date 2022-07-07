f8a7c947cf97aa25e4e68540ee3efa017817d802
Rahul Marathe <rahulvmarathe@live.com>
2022-07-07 16:21:09 -0400

    initial commit

---
+++ src/main/java/springappconfig/configkeyvault/ConfigkeyvaultApplication.java
@@ -0,0 +1,13 @@
+package springappconfig.configkeyvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigkeyvaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigkeyvaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springappconfig/configkeyvault/ConfigkeyvaultApplicationTests.java
@@ -0,0 +1,13 @@
+package springappconfig.configkeyvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigkeyvaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
