fd6853c180a6755f76371ab1e2f650e8b20348f6
Rahul Marathe <rahulvmarathe@live.com>
2022-07-07 21:34:43 +0000

    App configuration integration

--- src/main/java/springappconfig/configkeyvault/ConfigkeyvaultApplication.java
+++ src/main/java/springappconfig/configkeyvault/ConfigkeyvaultApplication.java
@@ -2,8 +2,10 @@ package springappconfig.configkeyvault;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(MessageProperties.class)
 public class ConfigkeyvaultApplication {
 
 	public static void main(String[] args) {
--- src/test/java/springappconfig/configkeyvault/ConfigkeyvaultApplicationTests.java
+++ src/test/java/springappconfig/configkeyvault/ConfigkeyvaultApplicationTests.java
@@ -3,7 +3,7 @@ package springappconfig.configkeyvault;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.cloud.azure.appconfiguration.enabled=false")
 class ConfigkeyvaultApplicationTests {
 
 	@Test
